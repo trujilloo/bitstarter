f5a7351675854aadcdf987c9b5b188df0a6a6bb3
Oscar Trujillo <trujilloo@gmail.com>
2013-08-08 03:09:39 +0000

    Added index.html

--- web.js
+++ web.js
@@ -1,12 +1,28 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+
+//OT not sure what this does? 8-7-13
+var fs = require('fs');
+
+//OT added for reading in file
+var htmlfile = "index.html";
+
+//OT commented out these two and replaced with this one
+//var app = express();
+var app = express.createServer(express.logger());
+//app.use(express.logger());
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+//OT replaced response send with file
+//  response.send('Hello World!');
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
+    
 });
 
-var port = process.env.PORT || 5000;
+//OT changed ports
+//var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
