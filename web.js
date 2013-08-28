ef78c5b49a08643636ec84c8579de009634fe134
Oscar Trujillo <trujilloo@gmail.com>
2013-08-28 03:23:07 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,24 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+//var app = express();
+
+//OT not sure what this does? 8-7-13
+var fs = require('fs');
+
+//OT added for reading in file
+var htmlfile = "index.html";
+
+var app = express.createServer(express.logger());
+
+//app.use(express.logger());
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
